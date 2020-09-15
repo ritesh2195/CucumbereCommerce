dfa7f64c097c5436da269c95cb72974f90fc1706
RITESH RANJAN MISHRA <58725105+ritesh2195@users.noreply.github.com>
2020-09-15 10:53:11 +0530

    Update runnerTest.java

--- src/test/java/Runner/runnerTest.java
+++ src/test/java/Runner/runnerTest.java
@@ -6,35 +6,60 @@ import java.time.LocalDateTime;
 //import com.vimalselvam.cucumber.listener.ExtentCucumberFormatter;
 //import com.vimalselvam.cucumber.listener.ExtentProperties;
 //import com.vimalselvam.cucumber.listener.Reporter;
+import com.vimalselvam.cucumber.listener.ExtentProperties;
+import com.vimalselvam.cucumber.listener.Reporter;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
-import com.cucumber.listener.ExtentCucumberFormatter;
+//import com.cucumber.listener.ExtentCucumberFormatter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin= {"html:target/cucumber_html_report", "pretty", "com.cucumber.listener.ExtentCucumberFormatter"},
-		features= {"features"},
-		glue= {"stepDefinition"}/*,
-		tags= {"@Login"}
+@CucumberOptions(plugin= {"pretty",
+		"html:out/cucumber-html-report","json:out/cucumber-report.json",
+		"usage:target/cucumber-usage.json",
+		"junit:target/cucumber-results.xml",
+		"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:"},
 
-*/
+		features= {"features"},
+
+		glue= {"stepDefinition"}
+
+		//tags= {"@Login"}
 
 )
 public class runnerTest {
 	@BeforeClass
 	public static void setup() {
 
-		ExtentCucumberFormatter.initiateExtentCucumberFormatter();
+		LocalDateTime date = LocalDateTime.now();
 
-		ExtentCucumberFormatter.loadConfig(new File("extent-config.xml"));
+		String fileName    = "target/report_";
 
-		ExtentCucumberFormatter.addSystemInfo("Browser name", "Chrome");
+		fileName           = fileName+date.toString().replace("-","_").replace(":","_")+ ".html";
 
-		ExtentCucumberFormatter.addSystemInfo("Browser version", "79");
+		ExtentProperties extentProperties = ExtentProperties.INSTANCE;
+
+		extentProperties.setReportPath(fileName);
+	}
+
+	@AfterClass
+	public static void teardown() {
+
+		Reporter.setSystemInfo("user", System.getProperty("user.name"));
+
+		Reporter.setSystemInfo("OS", "Windows 10");
+
+		Reporter.setSystemInfo("JAVA Version", "13");
+
+		Reporter.setSystemInfo("Browser name", "Chrome");
+
+		Reporter.setSystemInfo("Browser version", "85");
+
+		Reporter.setTestRunnerOutput("Sample test runner output message");
 
 
 	}
